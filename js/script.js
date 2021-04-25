c9fc30c8bc791ce209e6f47f9731a2820fd22695
as-syariahbordir <ndangsaepudin@gmail.com>
2021-04-25 20:22:01 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //<![CDATA[
-$('#Admin').val('0813-8626-7897');// Nomor Tujuan
+$('#Admin').val('0813-0000-0000');// Nomor Tujuan
 $('.whatsapp-btn').click(function () {
 $('#whatsapp').toggleClass('toggle');});
 // Fungsi Tombol Whatsapp Kirim!
