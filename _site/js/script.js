efcf5416b6dcff78e81b988c48001485786ef117
as-syariahbordir <ndangsaepudin@gmail.com>
2021-04-28 02:22:07 +0700

    update

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,52 +1,57 @@
 //<![CDATA[
-$('#Admin').val('0813-0000-0000');// Nomor Tujuan
+$('#Admin').val('0813-8626-7897');
 $('.whatsapp-btn').click(function () {
 $('#whatsapp').toggleClass('toggle');});
-// Fungsi Tombol Whatsapp Kirim!
+
 		$('#whatsapp .submit').click(WhatsApp);
 		$("#whatsapp input, #whatsapp textarea").keypress(function () {
 			if (event.which == 13) WhatsApp();
 		});
 		var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-// Fungsi Data Tulisan yang dikirim
+
 function WhatsApp() {
 			var ph = '';
-			if ($('#whatsapp .nama').val() == '') { // Cek Nama
+			if ($('#whatsapp .nama').val() == '') {
 				ph = $('#whatsapp .nama').attr('placeholder');
 				alert(ph + ' Wajib Diisi');
 				$('#whatsapp .nama').focus();
 				return false;
-			} else if ($('#whatsapp .alamat').val() == '') { // Cek alamat
+			} else if ($('#whatsapp .alamat').val() == '') {
 				ph = $('#whatsapp .alamat').attr('placeholder');
 				alert(ph + ' Mohon Diisi');
 				$('#whatsapp .alamat').focus();
                 return false;
-            } else if ($('#whatsapp .nomor').val() == '') { // Cek nomor
+            } else if ($('#whatsapp .nomor').val() == '') {
                 ph = $('#whatsapp .nomor').attr('placeholder');
                 alert(ph + ' Harus Terisi');
                 $('#whatsapp .nomor').focus();
 				return false;
-            } else if ($('#whatsapp .qty').val() == '') { // Cek jumlah
+            } else if ($('#whatsapp .qty').val() == '') {
                 ph = $('#whatsapp .qty').attr('placeholder');
                 alert('Jumlah Item Yang Di Beli Tidak Bisa ' + ph + ' pcs');
                 $('#whatsapp .qty').focus();
 				return false;
-            } else if ($('#whatsapp .bayar').val() == '') { // Cek bayar
+            } else if ($('#whatsapp .bayar').val() == '') {
 				ph = $('#whatsapp .bayar').attr('placeholder');
 				alert(ph + ' Metode?');
 				$('#whatsapp .bayar').focus();
 				return false;
-			} else if ($('#whatsapp .warna').val() == '') { // Cek warna
+			} else if ($('#whatsapp .warna').val() == '') {
 				ph = $('#whatsapp .warna').attr('placeholder');
 				alert('Silahkan pilih ' + ph);
 				$('#whatsapp .warna').focus();
 				return false;
-			} else if ($('#whatsapp .tali').val() == '') { // Cek tali
+			} else if ($('#whatsapp .tali').val() == '') {
 				ph = $('#whatsapp .tali').attr('placeholder');
 				alert('Silahkan pilih ' + ph);
 				$('#whatsapp .tali').focus();
 				return false;
-			} else if ($('#whatsapp .informasi').val() == '') { // Cek dari
+			} else if ($('#whatsapp .note').val() == '') {
+				ph = $('#whatsapp .note').attr('placeholder');
+				alert('Silahkan tambahkan ' + ph);
+				$('#whatsapp .note').focus();
+				return false;
+			} else if ($('#whatsapp .informasi').val() == '') {
 				ph = $('#whatsapp .informasi').attr('placeholder');
 				alert('Silahkan pilih ' + ph);
 				$('#whatsapp .informasi').focus();
@@ -67,6 +72,7 @@ function WhatsApp() {
 		            qty = $('#whatsapp .qty').val(),
                     warna = $('#whatsapp .warna').val(),
                     tali = $('#whatsapp .tali').val(),
+                    note = $('#whatsapp .note').val(),
 		            informasi = $('#whatsapp .informasi').val();
 				$(this).attr('href', url_wa + '?phone=62 ' + tujuan + '&text=' +
 
@@ -80,11 +86,11 @@ function WhatsApp() {
                '_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _%0A' +
                ' %0A%2AWarna Pilihan%2A: ' + warna +
                ' %0A%2AModel Tali%2A: ' + tali + '%0A' +
-               ' %0A%2AQty%2A: ' + qty + '%0A' +
+               ' %0A%2AQty%2A: ' + qty + ' pcs%0A' +
                ' %0A%2AJasa Kirim%2A: ' + informasi + '%0A' +
                '_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _%0A' +
                ' %2AOrder Lewat%2A ' + via_url + '%0A' +
-               ' %0A%2ACatatan%2A: ');
+               ' %0A%2ACatatan%2A: ' + note );
 				var w = 960,
 					h = 540,
 					left = Number((screen.width / 2) - (w / 2)),
