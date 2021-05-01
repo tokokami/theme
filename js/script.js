767e53dadab2c685a03679b39a92f9968d3871ff
daffadev-inc <ndangsaepudin@gmail.com>
2021-05-01 08:37:23 +0700

    update

--- js/script.js
+++ js/script.js
@@ -95,7 +95,7 @@ function WhatsApp() {
 					h = 540,
 					left = Number((screen.width / 2) - (w / 2)),
 					tops = Number((screen.height / 2) - (h / 2)),
-					popupWindow = window.open(this.href, '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=1, copyhistory=no, width=' + w + ', height=' + h + ', top=' + tops + ', left=' + left);
+					popupWindow = window.open(this.href, '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=1, copyhistory=no, width=' + w + ', height=' + h + ', top=' + tops + ', left=' + left);
                 popupWindow.focus();
 				return false;
 			}
